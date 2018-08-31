6db7eb6d884e24e413696f22e9b0f79e935ab3bb
blwzx <1049345539@qq.com>
2018-08-31 17:13:14 +0800

    pgy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const server = express();
 server.get('/',(req,res)=>{
     res.sendFile(
-        path.resolve('./wenzi/index.html')
+        path.resolve('./views/index.html')
     )
 })
 server.listen(80);
