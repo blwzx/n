3f89196d5b77393a7548ab5817bf816c3b508de0
blwzx <1049345539@qq.com>
2018-08-31 13:12:36 +0800

    saolei

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const path = ('path');
+const path = require('path');
 const server = express();
 server.get('/',(req,res)=>{
-    res.send('ok');
-    // res.sendFile(
-    //     path.resolve('./views/index.html')
-    // )
+    res.sendFile(
+        path.resolve('./views/index.html')
+    )
 })
-server.listen(80);
+server.listen(5000);
