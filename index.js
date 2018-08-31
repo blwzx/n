d84c25e394bdaa6650e34d5058779e8c387c3394
blwzx <1049345539@qq.com>
2018-08-31 11:40:35 +0800

    news

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = ('path');
+const server = express();
+server.get('/',(req,res)=>{
+    res.send('ok');
+    // res.sendFile(
+    //     path.resolve('./views/index.html')
+    // )
+});
+server.listen(80);
